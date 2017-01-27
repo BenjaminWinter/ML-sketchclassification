1a5e9e67b82580f459a4174fbc10c14b81c34c09
Benjamin Winter <Benny.Winter@yahoo.de>
2017-01-27 23:26:40 +0100

    Make TARGET_MAP dynamic

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
+import os
+
 FILE_TARGET = "./persistence/data.npy"
 FILE_DATA = "./persistence/targets.npy"
 IMGDB = "./db/**/*"
 CLASSIFIER = "./persistence/classifier.pkl"
-TARGET_MAP = {"apple":0,"bus":1,"calculator":2,
-              "envelope":3,"eyeglasses":4,"face":5,
-              "head-phones":6,"sailboat":7,"snowman":8,"t-shirt":9}
+
+dirs = [os.path.basename(x[0]) for x in os.walk("./db")]
+dirs.remove('db')
+TARGET_MAP = dict(zip(dirs, xrange(len(dirs))))
 TARGET_MAP_INVERSE = {v: k for k, v in TARGET_MAP.items()}
